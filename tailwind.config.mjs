fafe4abca9f9304b53b801852c5f6e4d197efee3
Angelo94a <angelo94.aa@gmail.com>
2024-03-15 20:00:36 -0500

    actualizando

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: { min: "320px", max: "767px" },
+      // => @media screen and (width >= 320px) and (width <= 767px)
+      md: { min: "768px", max: "884px" },
+      // => @media screen and (width >= 768px) and (width <= 1024px)
+      xl: { min: "1024px", max: "2560px" },
+    },
+  },
+  plugins: [],
+};
